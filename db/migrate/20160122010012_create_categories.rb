8527a9c5689a1c63c5c02c850eb8822711fd49cb
LAURA EVANS <LKSEVANS12@GMAIL.com>
2016-01-21 20:10:38 -0500

    fill in migrations

--- db/migrate/20160122010012_create_categories.rb
+++ db/migrate/20160122010012_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-
+      t.string :category_name, null:false
       t.timestamps null: false
     end
   end
--- db/migrate/20160122010037_create_ratings.rb
+++ db/migrate/20160122010037_create_ratings.rb
@@ -1,6 +1,12 @@
 class CreateRatings < ActiveRecord::Migration
   def change
     create_table :ratings do |t|
+      t.references :user,null:false
+      t.integer :reviewable_id
+      t.string :reviewable_type
+
+      t.text :review_body,null:false
+      t.integer :review_score,null:false
 
       t.timestamps null: false
     end
