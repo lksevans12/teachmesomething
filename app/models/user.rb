733a0c5e690b026c13c5829505144e889623fc9e
lksevans12 <lksevans12@gmail.com>
2016-02-13 15:28:22 -0500

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ActiveRecord::Base
 
   has_many :posts
 
-  validates_presence_of :email, :username, :password
+  PASSWORD_FORMAT = /\A
+  (?=.*\d)           # Must contain a digit
+  (?=.*[a-z])        # Must contain a lower case character
+  (?=.*[A-Z])        # Must contain an upper case character
+  /x
+
+  validates_presence_of :email, :username
+  validates :password, :length => {:within => 6..40},format: {with: PASSWORD_FORMAT},:on => :create
+  validates :password,:length => {:within => 6..40},format: {with: PASSWORD_FORMAT}, allow_nil: true ,:on => :update
   validates_uniqueness_of :email, :username
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
