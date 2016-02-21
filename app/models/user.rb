a13af2aacec9389747b68b0e90daa26a78726eaa
lksevans12 <lksevans12@gmail.com>
2016-02-20 21:33:16 -0500

    add dependency for posts and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   include Bootsy::Container
   has_secure_password
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_many :reviews, as: :reviewable, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_posts, through: :favorites, source: :favorited, source_type: "Post", dependent: :destroy
@@ -10,16 +10,11 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, styles: { large: "500x500>", small: "200x200>", thumb: "100x100>" }, default_url: "missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
-  PASSWORD_FORMAT = /\A
-  (?=.*\d)           # Must contain a digit
-  (?=.*[a-z])        # Must contain a lower case character
-  (?=.*[A-Z])        # Must contain an upper case character
-  /x
-
-  validates_presence_of :email, :username
-  validates :password, :length => {:within => 6..40},format: {with: PASSWORD_FORMAT},:on => :create
-  validates :password, :length => {:within => 6..40},format: {with: PASSWORD_FORMAT},allow_nil: true, :on => :update
+  validates_presence_of :email, :username, :password
+  validates :password, :length => {:within => 6..40},:on => :create
+  validates :password, :length => {:within => 6..40},allow_nil: true, :on => :update
   validates_uniqueness_of :email, :username
+  validates_format_of :password, :with => /^(?=.*[a-zA-Z])(?=.*[0-9]).{6,}$/,:multiline => true ,:on => :create ,:on => :update
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 
   def average_rating
